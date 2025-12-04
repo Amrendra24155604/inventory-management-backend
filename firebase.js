0551db19ecaa5aed8d8d9e40a78c08d89e2f66b4
Amrendra24155604 <amrendraky06@gmail.com>
2025-12-04 23:23:37 +0530

    firebase issue

--- firebase.js
+++ firebase.js
@@ -28,8 +28,9 @@ const firebaseConfig = {
 // firebase.js
 import admin from "firebase-admin";
 import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const serviceAccount = require("./firebase-service-account.json");
+// const require = createRequire(import.meta.url);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

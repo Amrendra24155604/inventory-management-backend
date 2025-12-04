7607a3092f68a1f6bd7dc1d5f58f4b87d86aa686
Amrendra24155604 <amrendraky06@gmail.com>
2025-12-05 00:23:25 +0530

    firebase issue 6

--- firebase.js
+++ firebase.js
@@ -1,57 +1,15 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// // import { getAuth } from "firebase/auth";
-// // import { initializeApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
-// // const firebaseConfig = {
-// //   apiKey: "AIzaSyBomi00puNHYy1niYI-IZzaXDEvLCTVzBU",
-// //   authDomain: "fir-basics-2cb88.firebaseapp.com",
-// //   projectId: "fir-basics-2cb88",
-// //   storageBucket: "fir-basics-2cb88.appspot.com",
-// //   messagingSenderId: "732616140926",
-// //   appId: "1:732616140926:web:38dcd67a91dd66501ad02f",
-// //   measurementId: "G-8K73GS8W4S",
-// //   databaseURL:"https://fir-basics-2cb88-default-rtdb.firebaseio.com/"
-// // };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBb8A2i7jQ7g5qZGT24FKQgBDvLHtdTt_o",
-//   authDomain: "invmanagement-774b6.firebaseapp.com",
-//   projectId: "invmanagement-774b6",
-//   storageBucket: "invmanagement-774b6.firebasestorage.app",
-//   messagingSenderId: "891974729305",
-//   appId: "1:891974729305:web:f7bdc44b57b9d13063fb48",
-//   measurementId: "G-VJRVCW7CPK"
-// };
-
-// // firebase.js
-// // firebase.js
-// import admin from "firebase-admin";
-// import { createRequire } from "module";
-// // const require = createRequire(import.meta.url);
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-// serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-// export default admin;
-
-// const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app);
-// const auth = getAuth(app);
-// const provider = new GoogleAuthProvider();
-// console.log("Auth instance:", auth);
-// console.log("Provider:", provider);
-// export { auth, provider };
-
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import admin from "firebase-admin";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const serviceAccount = JSON.parse(readFileSync(join(__dirname, "firebase-service-account.json"), "utf8"));
 
 // --- Client SDK config ---
 const firebaseConfig = {
@@ -70,17 +28,10 @@ export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 
 // --- Admin SDK config ---
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-  // Only needed if your .env stores \n escapes
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-  }
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
 export default admin;

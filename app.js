ad183e6615f3a71c292f249e79fdad1494c60d32
Amrendra24155604 <amrendraky06@gmail.com>
2025-12-05 22:16:41 +0530

    frontend url in cors

--- app.js
+++ app.js
@@ -18,9 +18,20 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-const allowedOrigins = ["http://localhost:5173", "http://10.192.218.149:5173"];
-app.use(cors({
-  origin: "http://localhost:5173",
+const allowedOrigins = [
+  "http://localhost:5173", // local dev
+  "https://inventory-management-frontend-5ioh.vercel.app", // deployed frontend
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   credentials: true
 }));
 app.options(/.*/, cors());
